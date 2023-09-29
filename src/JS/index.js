34098a3f023aead7dcfe9ec7a33b87b4b7b40a25
luis92guimaraes <luis92guimaraes@outlook.com>
2023-09-28 21:35:41 -0300

    ajuste style

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,6 +1,6 @@
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
 const body = document.querySelector("body");
-const imagemBotaoTrocaDeTema = document.querySelector("imagem-botao");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
 
 botaoAlterarTema.addEventListener("click", () => {
     const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
@@ -8,8 +8,8 @@ botaoAlterarTema.addEventListener("click", () => {
     body.classList.toggle("modo-escuro");
     
     if (modoEscuroEstaAtivo) {
-        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/hadouken.modoclaro.png");
+        imagemBotaoTrocaDeTema.setAttribute("src", "/src/imagens/hadouken.modoclaro.png");
     } else {
-        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/hadouken.modoescuro.png");
+        imagemBotaoTrocaDeTema.setAttribute("src", "/src/imagens/hadouken.modoescuro.png");
     }
 });
