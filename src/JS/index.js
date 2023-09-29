b3e519f4e54f16c741cc211321ee0a9b18b60e26
luis92guimaraes <luis92guimaraes@outlook.com>
2023-09-28 21:43:40 -0300

    ajuste botao hadouken

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,6 +1,6 @@
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
 const body = document.querySelector("body");
-const imagemBotaoTrocaDeTema = document.querySelector("imagem-botao");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
 
 botaoAlterarTema.addEventListener("click", () => {
     const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
